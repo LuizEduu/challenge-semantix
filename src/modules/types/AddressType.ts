a875a921cd0ab6a25a6ba98568d87ae1a0dec16c
Luiz <luiz.rocha@compasso.com.br>
2022-04-29 13:15:19 -0300

    feat: add Address type

---
+++ src/modules/types/AddressType.ts
@@ -0,0 +1,14 @@
+type Address = {
+
+  'street': string
+  'city': string
+  'state': string
+  'zipcode': string
+  'country': string
+  'number': string
+  'id': string
+  'userId': string
+
+}
+
+export { Address }
--- src/modules/users/useCases/getUserAddressUseCase/GetUserAddressUseCase.ts
+++ src/modules/users/useCases/getUserAddressUseCase/GetUserAddressUseCase.ts
@@ -1,10 +1,11 @@
+import { Address } from '@/modules/types/AddressType'
 import { User } from '@/modules/types/UserType'
 import axios from 'axios'
 import { injectable } from 'tsyringe'
 
 @injectable()
 class GetUserAddressUseCase {
-  async execute (user: User): Promise<any> {
+  async execute (user: User): Promise<Address> {
     const requestData = await axios.get(`${process.env.MOCKAPIBASEPATH as string}/users/${user.id}/address`)
     return requestData.data
   }

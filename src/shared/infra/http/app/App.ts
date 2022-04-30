f076f1248fb45ab793c08293430a9bb03cb16b76
Luiz <luiz.rocha@compasso.com.br>
2022-04-30 16:35:13 -0300

    doc: add swagger doc

--- src/shared/infra/http/app/App.ts
+++ src/shared/infra/http/app/App.ts
@@ -1,11 +1,16 @@
-
 import express from 'express'
 import errorMiddleware from '../middlewares/ErrorMiddleware'
 import { router } from '../routes'
 
+import swagger from '../../../../swagger.json'
+
+import swaggerUi from 'swagger-ui-express'
+
 const app = express()
 app.use(express.json())
 app.use(router)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger))
 app.use(errorMiddleware)
 
 export { app }
